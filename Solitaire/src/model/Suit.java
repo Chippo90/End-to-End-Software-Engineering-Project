690294c26f11f76d5a81523366874c09d8f907d5
Chehab Hany <chehabhany90@gmail.com>
2026-06-20 19:10:39 +0200

    Delete Solitaire directory

--- Solitaire/src/model/Suit.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-/**An enum representing the four suits in a standard deck of cards.
- *Enums are used here because the set of suits is fixed and known.*/
-public enum Suit {
-    HEARTS, DIAMONDS, CLUBS, SPADES
-}
